ac30c512696640f0bc9b841d2d78ea3cdf0acae8
sai-garimalla <kiran803534@gmail.com>
2026-01-12 16:57:10 +0530

    commit OddDigitsSum.java

---
+++ OddDigitsSum.java
@@ -0,0 +1,32 @@
+/*
+Odd Digits' Sum:
+
+In mathematics, the "digit sum" of a given integer is the sum of all its digits, e.g. the digit sum of 84001 is calculated as 8 + 4 + 0 + 0 + 1 = 13, the digit sum of 158 is 1 + 5 + 8 = 14.
+
+Rohan's teacher has asked him to write a function (method) that takes as input a positive number and performs digitSum of only the odd digits in the given number.
+
+Example 1: If the given number is 9625, we must add only the odd digits, i.e. 9 + 5 = 14. Thus, the OddDigitsSum for the number 9625 is 14.
+
+Example 2: If the given number is 2134, the OddDigitsSum will be 1 + 3 = 4.
+
+Assumption: The input number will be a positive integer number >= 1 and <= 25000. */
+
+import java.util.Scanner;
+
+class OddDigitsSum
+{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter a number: ");
+int n = sc.nextInt();
+int sum = 0;
+while (n > 0){
+ int digit = n % 10;
+if (digit % 2!= 0){
+sum = sum + digit;
+}
+n = n / 10;
+}
+System.out.println("Sum of odd digits = " + sum);
+    }
+}
